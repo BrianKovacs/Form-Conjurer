1d94ff200e2cd6589af6ee76604bcbc8bdfd52e0
BrianKovacs <brian.kovacs@me.com>
2013-11-30 15:32:49 -0500

    Added the Header class to the project.

---
+++ Header.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FormConjurerLib
+{
+    public class Header : Input
+    {
+        public override string Output(string input)
+        {
+            string OpenTag = "<h1 id=\"" + this.Id + "\" name=\"" + this.Name + "\" class=\"" + this.Class + "\">";
+            string CloseTag = "</h1>";
+
+            return OpenTag + input + CloseTag;
+        }
+
+        public Header(string Id)
+        {
+            this.Id = Id;
+            this.Name = string.Empty;
+            this.Class = string.Empty;
+        }
+        public Header(string Id, string Class)
+        {
+            this.Id = Id;
+            this.Class = Class;
+            this.Name = string.Empty;
+        }
+        public Header(string Id, string Class, string Name)
+        {
+            this.Class = Class;
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}

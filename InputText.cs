1e755eaddd7aa645cf8ea1a1991ad00a98457d3a
BrianKovacs <brian.kovacs@me.com>
2013-12-01 20:08:30 -0500

    Updated the InputText class with the Content property.

--- InputText.cs
+++ InputText.cs
@@ -7,15 +7,48 @@ namespace FormConjurerLib
 {
     public class InputText : Input
     {
-        public InputText(string Id)
+
+        public InputText(string Content)
         {
+            this.Content = Content;
+            this.Id = string.Empty;
+            this.Name = string.Empty;
+            this.Class = string.Empty;
+        }
+
+        public InputText(string Content, string Id)
+        {
+            this.Content = Content;
             this.Id = Id;
             this.Name = string.Empty;
             this.Class = string.Empty;
         }
+
+        public InputText(string Content, string Id, string Name)
+        {
+            this.Content = Content;
+            this.Id = Id;
+            this.Name = Name;
+            this.Class = string.Empty;
+        }
+
+        public InputText(string Content, string Id, string Name, string Class)
+        {
+            this.Content = Content;
+            this.Id = Id;
+            this.Name = Name;
+            this.Class = Class;
+        }
+
+        public void AddToContent(string input)
+        {
+            this.Content += input;
+        }
+
         public override string Output(string input)
         {
             return input + " <input type=\"text\" id= \"" + this.Id + "\" name=\"" + this.Name + "\" class=\"" + this.Class + "\">";
-        }  
+        }
+
     }
 }

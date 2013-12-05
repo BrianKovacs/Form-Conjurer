f92741b691144b917b8df1708592a1602172f880
Jargothon <taylor_charles28@hotmail.com>
2013-12-04 20:48:09 -0800

    Util Class for calling tesseract.dll methods

---
+++ TesseractUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using tessnet2;
+
+namespace FormConjurerLib
+{
+    public class TesseractUtil
+    {
+        public void Return(string path)
+        {   
+            
+            Tesseract tesseract = new Tesseract();
+            Bitmap bitmap = new Bitmap(path);
+            Point start = new Point(0,0);
+            Rectangle rect = new Rectangle(start,bitmap.Size);
+            var output = tesseract.DoOCR(bitmap, rect);
+            string text = string.Empty;
+            foreach (Word a in output)
+            {
+               text += a.Text;
+
+            }
+            int b = 0;
+
+        }
+    }
+}
